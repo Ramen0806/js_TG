f77cae1449fcc0b02c0475458d79fc41df94f09d
Ramen0806 <ramezkarim2244@gmail.com>
2023-09-27 13:16:07 -0500

    hmw

--- Homework/Homework8.js
+++ Homework/Homework8.js
@@ -252,13 +252,13 @@ function noXInVariables$(arr) {
         return result;
       }
   
-      
- const noXInVariables = arr =>{
-        removedArr = arr.map(word =>{
-           return (word.toString().split("").filter(letter =>!(letter.toUpperCase() === "X"))).join("")
-        })
-        return removedArr.filter(word => (word.length > 0))
-    }
+
+ const noXInVariables = arr => {
+  removedArr = arr.map(word => {
+         return (word.toString().split("").filter(letter =>!(letter.toUpperCase() === "X"))).join("")
+        });
+     return removedArr.filter(word => (word.length > 0))
+}
       
      
 console.log(noXInVariables(["abc", 123, "#$%"]));
@@ -266,4 +266,7 @@ console.log(noXInVariables(["xyz", 123, "#$%"]));
 console.log(noXInVariables(["x", 123, "#$%"]))
 console.log(noXInVariables(["xyXyxy", "Xx", "ABC"]));
 
-
+console.log(noXInVariables$(["abc", 123, "#$%"]));
+console.log(noXInVariables$(["xyz", 123, "#$%"]));
+console.log(noXInVariables$(["x", 123, "#$%"]))
+console.log(noXInVariables$(["xyXyxy", "Xx", "ABC"]));
--- Homework/tempCodeRunnerFile.js
+++ Homework/tempCodeRunnerFile.js
@@ -1,12 +1,34 @@
- const noXInVariables = arr =>{
-        removedArr = arr.map(word =>{
-           return (word.toString().split("").filter(letter =>!(letter.toUpperCase() === "X"))).join("")
-        })
-        return removedArr.filter(word => (word.length > 0))
-    }
+function noXInVariables$(arr) {
+        let result = [];
+        for (let i = 0; i < arr.length; i++) {
+          const element = arr[i];
+          if (typeof element === "string") {
+            const mElement = element.split('').filter(char => char.toLowerCase() !== 'x').join('');
+            if (mElement !== "") {
+              result.push(mElement);
+            }
+          } else {
+            result.push(element);
+          }
+        }
+        return result;
+      }
+  
+
+ const noXInVariables = arr => {
+  removedArr = arr.map(word => {
+         return (word.toString().split("").filter(letter =>!(letter.toUpperCase() === "X"))).join("")
+        });
+     return removedArr.filter(word => (word.length > 0))
+}
       
      
 console.log(noXInVariables(["abc", 123, "#$%"]));
 console.log(noXInVariables(["xyz", 123, "#$%"]));
 console.log(noXInVariables(["x", 123, "#$%"]))
 console.log(noXInVariables(["xyXyxy", "Xx", "ABC"]));
+
+console.log(noXInVariables$(["abc", 123, "#$%"]));
+console.log(noXInVariables$(["xyz", 123, "#$%"]));
+console.log(noXInVariables$(["x", 123, "#$%"]))
+console.log(noXInVariables$(["xyXyxy", "Xx", "ABC"]));
